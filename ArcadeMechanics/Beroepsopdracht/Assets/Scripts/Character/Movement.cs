3abb947339c72555139802df7fb4eaa3dc4a2a5c
Jayden <macbeanjayden@gmail.com>
2021-02-26 16:37:00 +0100

    Beroepsopdracht scene added

--- ArcadeMechanics/Beroepsopdracht/Assets/Scripts/Character/Movement.cs
+++ ArcadeMechanics/Beroepsopdracht/Assets/Scripts/Character/Movement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    private Rigidbody2D _rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
