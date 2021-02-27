2c8fed4d6a197483e43627ac5047a1e38a0aa8b5
Jayden <macbeanjayden@gmail.com>
2021-02-27 22:06:07 +0100

    Walking anims added, semi functional jump anim

--- ArcadeMechanics/Beroepsopdracht/Assets/Scripts/Character/Movement.cs
+++ ArcadeMechanics/Beroepsopdracht/Assets/Scripts/Character/Movement.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Character_Movement
+namespace movement_Character
 {
     public class Movement : MonoBehaviour
     {
-
-        private Rigidbody2D _rigidbody;
+        bool Is_Walking_Backward;
+        bool Is_Walking_Forward;
+        bool Is_Jumping;
+        protected Rigidbody2D _rb2D;
         public float JumpForce = 1f;
+        public float speed = 7.0f;
+        Animator cAnim;
         // Start is called before the first frame update
         void Start()
         {
-            _rigidbody = GetComponent<Rigidbody2D>();
-            
+            _rb2D = GetComponent<Rigidbody2D>();
+            cAnim = gameObject.GetComponent<Animator>();
+            Is_Walking_Backward = false;
+            Is_Walking_Forward = false;
+            Is_Jumping = false;
         }
 
         // Update is called once per frame
@@ -21,17 +28,50 @@ namespace Character_Movement
         {
             if (Input.GetKey(KeyCode.A))
             {
-                transform.position = transform.position + new Vector3(-0.045f, 0, 0);
+                Is_Walking_Backward = true;
+                transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
+                if(Is_Walking_Backward == true)
+                {
+                    cAnim.SetBool("Is_Walking_Backward", true);
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                Is_Walking_Backward = false;
+                cAnim.SetBool("Is_Walking_Backward", false);
             }
 
             if (Input.GetKey(KeyCode.D))
             {
-                transform.position = transform.position + new Vector3(0.045f, 0, 0);
+                Is_Walking_Forward = true;
+                transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+                if(Is_Walking_Forward == true)
+                {
+                    cAnim.SetBool("Is_Walking_Forward", true);
+                }
             }
 
-            if (Input.GetKeyDown(KeyCode.I) && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+            if(Input.GetKeyUp(KeyCode.D))
             {
-                _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+                Is_Walking_Forward = false;
+                cAnim.SetBool("Is_Walking_Forward", false);
+            }
+
+            if (Input.GetKeyDown(KeyCode.I) && Mathf.Abs(_rb2D.velocity.y) < 0.001f)
+            {
+                Is_Jumping = true;
+                _rb2D.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+                if(Is_Jumping == true)
+                {
+                    cAnim.SetBool("Is_Jumping", true);
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.I))
+            {
+                Is_Jumping = false;
+                cAnim.SetBool("Is_Jumping", false);
             }
         }
     }
