860bab0a8744b81d1c6aff9ad85a61a96bbd59b2
Jayden <macbeanjayden@gmail.com>
2021-02-27 14:47:42 +0100

    Changed jump button to KeyCode.I, functional idle anim for test character

--- ArcadeMechanics/Beroepsopdracht/Assets/Scripts/Character/Movement.cs
+++ ArcadeMechanics/Beroepsopdracht/Assets/Scripts/Character/Movement.cs
@@ -2,33 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+namespace Character_Movement
 {
-
-    private Rigidbody2D _rigidbody;
-    public float JumpForce = 1f;
-    // Start is called before the first frame update
-    void Start()
+    public class Movement : MonoBehaviour
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.A))
+        private Rigidbody2D _rigidbody;
+        public float JumpForce = 1f;
+        // Start is called before the first frame update
+        void Start()
         {
-            transform.position = transform.position + new Vector3(-0.045f, 0, 0);
+            _rigidbody = GetComponent<Rigidbody2D>();
+            
         }
 
-        if (Input.GetKey(KeyCode.D))
+        // Update is called once per frame
+        void Update()
         {
-            transform.position = transform.position + new Vector3(0.045f, 0, 0);
-        }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position = transform.position + new Vector3(-0.045f, 0, 0);
+            }
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
-        {
-            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position = transform.position + new Vector3(0.045f, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.I) && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+            {
+                _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            }
         }
     }
 }
+ 
