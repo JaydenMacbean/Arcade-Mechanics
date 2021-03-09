51252bdc7b00447335f80799b15d19f23808cdc1
Sherk-cell <22826@ma-web.nl>
2021-03-09 12:50:38 +0100

    healthbar added

---
+++ Healthbar/Assets/damage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damage : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public healthbar healthbar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+     void Update()
+    {
+     if (Input.GetKeyDown(KeyCode.Space))
+      {
+            Takedamage(20);
+        }
+    }
+    void Takedamage(int damage)
+    {
+        currentHealth -= damage;
+        healthbar.setHealth(currentHealth);
+    }
+
+
+
+
+}
+
