def0f2fd9ee2445808e20b8756bd91ea4c68d4f3
Jayden <macbeanjayden@gmail.com>
2021-03-23 15:55:45 +0100

    Seeable healthbar, added a Death if statement

---
+++ ArcadeMechanics/Beroepsopdracht/Assets/Scripts/UI/healthbar/damage.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class damage : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public healthbar healthbar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+     void Update()
+     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Takedamage(20);
+        }
+
+        if (currentHealth == 0)
+        {
+            Death(0);
+        }
+     }
+    void Takedamage(int damage)
+    {
+        currentHealth -= damage;
+        healthbar.setHealth(currentHealth);
+    }
+
+    void BackToScene(string Scene)
+    {
+        
+        SceneManager.LoadScene(Scene);
+    }
+    void Death(int damage)
+    {
+        if (currentHealth == 0)
+        {
+            BackToScene("RoundhouseScene");
+            currentHealth = 100;
+        }
+    }
+
+
+
+
+}
+
