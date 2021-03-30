6884cbe5c918ed5324b2f958c47e3ffa27d3528c
Jayden <macbeanjayden@gmail.com>
2021-03-30 12:18:48 +0200

    P2 Healthbar added (made by Byron)"

---
+++ ArcadeMechanics/Beroepsopdracht/Assets/Scripts/UI/healthbar/P2Damage.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class P2Damage : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public healthbar healthbar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Takedamage(20);
+        }
+
+        if (currentHealth == 0)
+        {
+            Death(0);
+        }
+    }
+    void Takedamage(int damage)
+    {
+        currentHealth -= damage;
+        healthbar.setHealth(currentHealth);
+    }
+
+    void BackToScene(string Scene)
+    {
+
+        SceneManager.LoadScene(Scene);
+    }
+    void Death(int damage)
+    {
+        if (currentHealth == 0)
+        {
+            BackToScene("RoundhouseScene");
+            currentHealth = 100;
+        }
+    }
+}
